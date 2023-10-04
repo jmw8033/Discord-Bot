fe4ac5b575bbee00576c8fbe54dc30a7b74610ac
Billy Soup <joeythesoda@gmail.com>
2023-10-04 13:09:50 -0400

    can now join voice channel

--- DISCORDBOT2023.py
+++ DISCORDBOT2023.py
@@ -14,6 +14,7 @@ GENERAL_CHANNEL_ID = 850841282485289015
 GAUSS_MEAN = 8000
 GAUSS_STD = 20000
 MESSAGE_LIMIT = None
+INTENTS = True
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -25,9 +26,11 @@ class MyClient(discord.Client):
         self.time_to_wait = None
         await client.change_presence(activity=discord.Game("Getting Bitches Simulator 2023"))            
         await self.initialize_msg_list()
-        print("Initializing intents...")
         self.initialized = True
-        self.myintents = myintents.MyIntents(self.msg_list)
+        if INTENTS:
+            print("Initializing intents...")
+            self.myintents = myintents.MyIntents(self.msg_list)
+            print("Intents initialized")
         await client.change_presence(activity=discord.Streaming(name="Shark Tank", url="https://www.twitch.tv/gothamchess"))
         self.msg_task = self.loop.create_task(self.msg_loop()) # start message loop
 
@@ -62,8 +65,16 @@ class MyClient(discord.Client):
                 await message.channel.send(f"{self.time_left} minutes remain", reference=message)
                 return
             
-            response = self.myintents.get_response(message)
-            await message.channel.send(response, reference=message)
+            if message.content.lower().endswith("join"):
+                voice_state = message.author.voice
+                if voice_state and self.guild.voice_client not in self.voice_clients:
+                    voice = await voice_state.channel.connect()
+                    self.sound_task = self.loop.create_task(self.play_random_sound(voice))
+                return
+            
+            if INTENTS:
+                response = self.myintents.get_response(message)
+                await message.channel.send(response, reference=message)
         elif dice < 5:
             await message.channel.send(mytenorpy.search_tenor(message.content), reference=message)
 
@@ -72,7 +83,8 @@ class MyClient(discord.Client):
         counter = random.randint(1, 100)
         channel = self.get_channel(GENERAL_CHANNEL_ID) # general chat
         while not self.is_closed():
-            await self.wait_random_time()
+            self.start_wait_time = datetime.datetime.now()
+            await asyncio.sleep(self.wait_random_time())
             await self.send_rand_message(channel, counter)
 
     async def send_rand_message(self, channel, counter):
@@ -89,17 +101,36 @@ class MyClient(discord.Client):
                 await channel.send(self.rand_message)
             counter += 1
 
-    async def wait_random_time(self):
+    async def wait_random_time(self, gauss_mean=GAUSS_MEAN, gauss_std=GAUSS_STD, min_wait=MIN_WAIT, max_wait=MAX_WAIT):
             # wait for a random time
-            time_to_wait = max(min(abs(random.gauss(GAUSS_MEAN, GAUSS_STD)), MAX_WAIT), MIN_WAIT)
-            self.start_wait_time = datetime.datetime.now()
-            self.time_to_wait = time_to_wait
-            print(f"Waiting {time_to_wait} seconds")
-            await asyncio.sleep(time_to_wait)
+            self.time_to_wait = max(min(abs(random.gauss(gauss_mean, gauss_std)), max_wait), min_wait)
+            print(f"Waiting {self.time_to_wait} seconds")
+            return self.time_to_wait
 
     async def get_reply_author(self, message):
         return None if message.reference is None or client.get_channel(message.reference.channel_id) is None  \
                     else (await client.get_channel(message.reference.channel_id).fetch_message(message.reference.message_id)).author
+    
+    async def play_random_sound(self, voice):
+        if not voice.is_connected() and self.sound_task is not None:
+            self.sound_task.cancel()
+            return
+        if voice.is_connected() and len(voice.channel.members) == 1:
+            await voice.disconnect()
+            self.sound_task.cancel()
+            return
+        
+        sound_files = [(f"{config.SOUND_DIR}/{x}.mp3", y) for x, y in [
+            ("A60", 5), ("AMBUSH", 5), ("AMBUSH2", 5), ("FIGURE", 10), ("FIGURE2", 10), ("HALT", 5),
+            ("HIDE", 10), ("JACK", 2), ("JEFF", 20), ("PSST", 40), ("SCREECH", 30), ("SEEK", 10), ("SEEK2", 10),
+            ("TIMOTHY", 5), ("RUSH", 10), ("ELEVATOR", 1), ("FNAF", 2)]]
+        values = [x for x, y in sound_files]
+        weights = [y for x, y in sound_files]
+        sound = random.choices(values, weights)[0]
+
+        voice.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe", source=sound))
+        print(f"Playing {sound}")
+        await asyncio.sleep(await self.wait_random_time(2000, 5000, 200, 20000))
 
     @property
     def rand_message(self):
